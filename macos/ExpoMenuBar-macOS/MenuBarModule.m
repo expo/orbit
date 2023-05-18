68f4a172261f1452c6d30b7773f83d0d9e672156
Gabriel Donadel <donadeldev@gmail.com>
2023-05-18 11:48:29 -0300

    Add support for running terminal commands from RN

--- macos/ExpoMenuBar-macOS/MenuBarModule.m
+++ macos/ExpoMenuBar-macOS/MenuBarModule.m
@@ -11,4 +11,27 @@ RCT_EXPORT_METHOD(exitApp)
   exit(0);
 }
 
+RCT_EXPORT_METHOD(runCommand:(NSString *)command
+                  arguments:(NSArray *)arguments
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  NSTask *task = [[NSTask alloc] init];
+  NSPipe *pipe = [NSPipe pipe];
+
+  [task setLaunchPath:@"/bin/sh"];
+  [task setArguments:@[@"-l", @"-c", [NSString stringWithFormat:@"PATH=\"$PATH:/usr/local/bin\"; %@ %@ ", command, [arguments componentsJoinedByString:@" "]]]];
+
+  [task setStandardOutput:pipe];
+  [task setStandardError:pipe];
+
+  NSFileHandle *file = [pipe fileHandleForReading];
+  [task launch];
+
+  NSData *data = [file readDataToEndOfFile];
+  NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
+  resolve(output);
+}
+
 @end
--- src/MenuBarModule.ts
+++ src/MenuBarModule.ts
@@ -2,6 +2,7 @@ import {NativeModules} from 'react-native';
 
 type MenuBarModule = {
   exitApp(): void;
+  runCommand: (command: string, args: string[]) => Promise<string>;
 };
 
 export default NativeModules.MenuBarModule as MenuBarModule;
