1e5f5cc2896997f151f13f77a559ddd6e44497c5
Gabriel Donadel <donadeldev@gmail.com>
2023-05-23 18:43:04 -0300

    Update workspace config to no longer require nohoist

--- apps/expo-menu-bar/metro.config.js
+++ apps/expo-menu-bar/metro.config.js
@@ -5,7 +5,19 @@
  * @format
  */
 
+const path = require('path');
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
 module.exports = {
+  watchFolders: [workspaceRoot],
+  resolver: {
+    disableHierarchicalLookup: true,
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
