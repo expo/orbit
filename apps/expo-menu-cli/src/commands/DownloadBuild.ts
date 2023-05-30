11437e802f79ebaaaa71c2339975778b649edc50
Gabriel Donadel <donadeldev@gmail.com>
2023-05-30 15:48:07 -0300

    [menu-cli] Fix donwload-build command to work with Android and iOS

--- apps/expo-menu-cli/src/commands/DownloadBuild.ts
+++ apps/expo-menu-cli/src/commands/DownloadBuild.ts
@@ -3,7 +3,7 @@ import { downloadAndMaybeExtractAppAsync, AppPlatform } from "eas-shared";
 export async function downloadBuild(buildURL: string) {
   const buildPath = await downloadAndMaybeExtractAppAsync(
     buildURL,
-    AppPlatform.Ios
+    buildURL.endsWith("apk") ? AppPlatform.Android : AppPlatform.Ios
   );
   return buildPath;
 }
