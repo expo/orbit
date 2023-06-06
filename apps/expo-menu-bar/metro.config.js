a38fd31cd60c31c072c78a5e883ae611b6bdf0c6
Gabriel Donadel <donadeldev@gmail.com>
2023-06-06 20:47:35 -0300

    [menu-bar] Add windows folder

--- apps/expo-menu-bar/metro.config.js
+++ apps/expo-menu-bar/metro.config.js
@@ -4,19 +4,26 @@
  *
  * @format
  */
-
+const fs = require('fs');
 const path = require('path');
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const rnwPath = fs.realpathSync(
+  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+);
 
 module.exports = {
-  watchFolders: [workspaceRoot],
   resolver: {
-    disableHierarchicalLookup: true,
-    nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(workspaceRoot, 'node_modules'),
-    ],
+    blockList: exclusionList([
+      // This stops "react-native run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      new RegExp(`${rnwPath}/build/.*`),
+      new RegExp(`${rnwPath}/target/.*`),
+      /.*\.ProjectImports\.zip/,
+    ]),
   },
   transformer: {
     getTransformOptions: async () => ({
---
+++ apps/expo-menu-bar/windows/ExpoMenuBar/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ExpoMenuBar::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ExpoMenuBar::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
