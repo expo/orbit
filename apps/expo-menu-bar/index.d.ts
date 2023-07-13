843f9575c42f00c4197eb757da5e86ae22698199
Gabriel Donadel <donadeldev@gmail.com>
2023-07-13 19:21:04 -0300

    [menu-bar] Add react-native-svg-transformer

--- apps/expo-menu-bar/index.d.ts
+++ apps/expo-menu-bar/index.d.ts
@@ -1,2 +1,8 @@
 declare module '*.jpg';
 declare module '*.png';
+declare module '*.svg' {
+  import React from 'react';
+  import {SvgProps} from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
--- apps/expo-menu-bar/metro.config.js
+++ apps/expo-menu-bar/metro.config.js
@@ -8,22 +8,31 @@
 const path = require('path');
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  watchFolders: [workspaceRoot],
-  resolver: {
-    disableHierarchicalLookup: true,
-    nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(workspaceRoot, 'node_modules'),
-    ],
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    watchFolders: [workspaceRoot],
+    resolver: {
+      disableHierarchicalLookup: true,
+      nodeModulesPaths: [
+        path.resolve(projectRoot, 'node_modules'),
+        path.resolve(workspaceRoot, 'node_modules'),
+      ],
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+  };
+})();
