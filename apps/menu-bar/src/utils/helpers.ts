e1bfdd96133b2a6edfcf158d93941d704d502b4d
Gabriel Donadel <donadeldev@gmail.com>
2023-08-08 13:21:55 -0300

    [menu-bar] Update Devices list to use a SectionList

--- apps/menu-bar/src/utils/helpers.ts
+++ apps/menu-bar/src/utils/helpers.ts
@@ -1,10 +1,3 @@
-export function partition<T>(array: T[], predicate: (item: T) => boolean) {
-  return array.reduce(
-    (acc: T[][], item: T) => (acc[+!predicate(item)].push(item), acc),
-    [[], []],
-  );
-}
-
 export function capitalize(word: string) {
   return `${word.toUpperCase()[0]}${word.substring(1)}`;
 }
