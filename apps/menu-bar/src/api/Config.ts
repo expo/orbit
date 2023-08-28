33ac874fe6826159c9b21e964624ee9dc10d1ebb
Gabriel Donadel <donadeldev@gmail.com>
2023-08-28 11:25:46 -0300

    [menu-bar] Add Login button to settings window

---
+++ apps/menu-bar/src/api/Config.ts
@@ -0,0 +1,13 @@
+const host = 'exp.host';
+const origin = `https://${host}`;
+const websiteOrigin = 'https://expo.dev';
+
+export default {
+  api: {
+    host,
+    origin,
+  },
+  website: {
+    origin: websiteOrigin,
+  },
+};
