39c0d93fd6da38825f075d7f148b2f3ab403f040
Gabriel Donadel <donadeldev@gmail.com>
2023-08-28 14:05:14 -0300

    [menu-bar] Setup apollo client

---
+++ apps/menu-bar/src/api/ApolloClient.ts
@@ -0,0 +1,93 @@
+import {
+  ApolloClient,
+  FieldFunctionOptions,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { HttpLink } from '@apollo/client/link/http';
+import { MMKVWrapper, persistCache } from 'apollo3-cache-persist';
+import { useEffect, useState } from 'react';
+
+import Config from './Config';
+import { storage } from '../modules/Storage';
+
+const httpLink = new HttpLink({
+  uri: `${Config.api.origin}/--/graphql`,
+});
+
+const mergeBasedOnOffset = (existing: any[], incoming: any[], { args }: FieldFunctionOptions) => {
+  const merged = existing ? existing.slice(0) : [];
+
+  for (let i = 0; i < incoming.length; ++i) {
+    merged[i + (args?.offset || 0)] = incoming[i];
+  }
+  return merged;
+};
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    AppQuery: {
+      keyFields: ['byId', ['id']],
+    },
+    AccountQuery: {
+      keyFields: ['byId', ['id']],
+    },
+    Account: {
+      fields: {
+        apps: {
+          keyArgs: ['limit'],
+          merge: mergeBasedOnOffset,
+        },
+      },
+    },
+    App: {
+      keyFields: ['id'],
+      fields: {
+        builds: {
+          keyArgs: ['limit', 'platform'],
+          merge: mergeBasedOnOffset,
+        },
+      },
+    },
+  },
+});
+
+interface UseApolloClientParams {
+  sessionSecret?: string;
+}
+
+export const useApolloClient = ({ sessionSecret }: UseApolloClientParams) => {
+  const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
+
+  useEffect(() => {
+    async function init() {
+      await persistCache({
+        cache,
+        storage: new MMKVWrapper(storage),
+        key: 'apollo-cache-persist',
+      });
+
+      const authMiddlewareLink = setContext(() => {
+        if (sessionSecret) {
+          return {
+            headers: { 'expo-session': sessionSecret },
+          };
+        }
+      });
+
+      setClient(
+        new ApolloClient({
+          link: authMiddlewareLink.concat(httpLink),
+          cache,
+        })
+      );
+    }
+
+    init();
+  }, [sessionSecret]);
+
+  return {
+    client,
+  };
+};
