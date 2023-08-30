b17b10f9e44abaa22846ec766c2b46394a7a6614
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2023-08-30 17:40:57 -0300

    [menu-bar] Add custom prompt support to FilePicker module (#47)
    * [menu-bar] Add custom prompt support to FilePicker module
    
    * [menu-bar] Update FilePicker getAppAsync propmt text

--- apps/menu-bar/src/modules/FilePickerModule.ts
+++ apps/menu-bar/src/modules/FilePickerModule.ts
@@ -1,7 +1,7 @@
 import { NativeModule, NativeModules } from 'react-native';
 
 type FilePickerModuleType = NativeModule & {
-  pickFileWithFilenameExtension: (extensions: string[]) => Promise<string>;
+  pickFileWithFilenameExtension: (extensions: string[], prompt?: string) => Promise<string>;
   pickFolder: () => Promise<string>;
 };
 
@@ -10,7 +10,10 @@ const FilePickerModule: FilePickerModuleType = NativeModules.FilePicker;
 export default {
   ...FilePickerModule,
   getAppAsync: () => {
-    return FilePickerModule.pickFileWithFilenameExtension(['apk', 'app', 'gzip', 'ipa', 'tar']);
+    return FilePickerModule.pickFileWithFilenameExtension(
+      ['apk', 'app', 'gzip', 'ipa', 'tar'],
+      'Select'
+    );
   },
   pickFolder: async () => FilePickerModule.pickFolder(),
 };
