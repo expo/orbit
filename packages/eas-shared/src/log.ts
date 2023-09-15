5e431332fee4ab448a5489cffa1ae8c235834555
Gabriel Donadel <donadeldev@gmail.com>
2023-09-15 09:31:07 -0300

    [eas-shared] Remove unnecessary dependencies

--- packages/eas-shared/src/log.ts
+++ packages/eas-shared/src/log.ts
@@ -1,8 +1,6 @@
 import chalk from "chalk";
-import figures from "figures";
 import { boolish } from "getenv";
 import logSymbols from "log-symbols";
-import terminalLink from "terminal-link";
 
 type Color = (...text: string[]) => string;
 
@@ -53,10 +51,6 @@ export default class Log {
     Log.log(`${chalk.green(logSymbols.success)} ${message}`);
   }
 
-  public static withTick(...args: any[]): void {
-    Log.consoleLog(chalk.green(figures.tick), ...args);
-  }
-
   private static consoleLog(...args: any[]): void {
     Log.updateIsLastLineNewLine(args);
     // eslint-disable-next-line no-console
@@ -83,47 +77,6 @@ export default class Log {
       }
     }
   }
-}
-
-/**
- * Prints a link for given URL, using text if provided, otherwise text is just the URL.
- * Format links as dim (unless disabled) and with an underline.
- *
- * @example https://expo.dev
- */
-export function link(
-  url: string,
-  {
-    text = url,
-    fallback,
-    dim = true,
-  }: { text?: string; dim?: boolean; fallback?: string } = {}
-): string {
-  // Links can be disabled via env variables https://github.com/jamestalmage/supports-hyperlinks/blob/master/index.js
-  const output = terminalLink(text, url, {
-    fallback: () =>
-      fallback ??
-      (text === url
-        ? chalk.underline(url)
-        : `${text}: ${chalk.underline(url)}`),
-  });
-  return dim ? chalk.dim(output) : output;
-}
-
-/**
- * Provide a consistent "Learn more" link experience.
- * Format links as dim (unless disabled) with an underline.
- *
- * @example Learn more: https://expo.dev
- */
-export function learnMore(
-  url: string,
-  {
-    learnMoreMessage: maybeLearnMoreMessage,
-    dim = true,
-  }: { learnMoreMessage?: string; dim?: boolean } = {}
-): string {
-  return link(url, { text: maybeLearnMoreMessage ?? "Learn more", dim });
 }
 
 export function log(...message: string[]): void {
