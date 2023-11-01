92d476946c302f65e7a537c28fcf2e7323f1509e
Gabriel Donadel <donadeldev@gmail.com>
2023-11-01 17:50:21 -0300

    [menu-bar] Fix useDeepLinking hook initial URL

--- apps/menu-bar/src/hooks/useDeepLinking.ts
+++ apps/menu-bar/src/hooks/useDeepLinking.ts
@@ -1,11 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Linking } from 'react-native';
 
 export type DeepLinkingCallback = (event: { url: string }) => void;
 
 export const useDeepLinking = (callback: DeepLinkingCallback) => {
+  const [initialURL, setInitialURL] = useState<string | null>(null);
+
   useEffect(() => {
-    Linking.getInitialURL().then((url) => url && callback({ url }));
+    Linking.getInitialURL().then(setInitialURL);
+  }, []);
+
+  if (initialURL) {
+    callback({ url: initialURL });
+    setInitialURL(null);
+  }
+
+  useEffect(() => {
     const listener = Linking.addEventListener('url', callback);
 
     return listener.remove;
