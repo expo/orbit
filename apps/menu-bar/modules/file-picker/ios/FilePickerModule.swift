88f8604e4da52f29dfb57c845c8b75cf7dc67012
Gabriel Donadel <donadeldev@gmail.com>
2024-01-10 10:39:15 -0300

    Use runOnQueue instead of DispatchQueue

--- apps/menu-bar/modules/file-picker/ios/FilePickerModule.swift
+++ apps/menu-bar/modules/file-picker/ios/FilePickerModule.swift
@@ -5,7 +5,7 @@ public class FilePickerModule: Module {
     public func definition() -> ModuleDefinition {
         Name("FilePicker")
 
-        AsyncFunction("pickFileWithFilenameExtension"){  (filenameExtensions: [String], prompt: String) in
+        AsyncFunction("pickFileWithFilenameExtension"){ (filenameExtensions: [String], prompt: String) -> String? in
               NSApp.activate(ignoringOtherApps: true)
               let panel = NSOpenPanel()
 
@@ -26,35 +26,31 @@ public class FilePickerModule: Module {
                           allowedTypes += utTypesFromTag
                       }
 
-
                       if let utTypeFromFilenameExtension = UTType(filenameExtension: extensionString), !allowedTypes.contains(utTypeFromFilenameExtension) {
                           allowedTypes.append(utTypeFromFilenameExtension)
                       }
-                  } 
+                  }
 
                   panel.allowedContentTypes = allowedTypes
               }
 
               guard panel.runModal() == NSApplication.ModalResponse.OK else {
                    throw Exception(name: "FILE_PICKER_ERROR", description: "NSModalResponseCancel")
-              } 
+              }
               return panel.url?.path
         }.runOnQueue(.main)
 
-        AsyncFunction("pickFolder"){  (promise: Promise) in
-            DispatchQueue.main.async {
-                let panel = NSOpenPanel()
-                panel.allowsMultipleSelection = false
-                panel.canChooseFiles = false
-                panel.canChooseDirectories = true
-                panel.canCreateDirectories = true
+        AsyncFunction("pickFolder"){ () -> String? in
+            let panel = NSOpenPanel()
+            panel.allowsMultipleSelection = false
+            panel.canChooseFiles = false
+            panel.canChooseDirectories = true
+            panel.canCreateDirectories = true
 
-                if panel.runModal() == NSApplication.ModalResponse.OK {
-                    promise.resolve(panel.url?.path)
-                } else {
-                    promise.reject(Exception(name: "FILE_PICKER_ERROR", description: "NSModalResponseCancel"))
-                }
+            guard panel.runModal() == NSApplication.ModalResponse.OK else {
+                throw Exception(name: "FILE_PICKER_ERROR", description: "NSModalResponseCancel")
             }
-        }
+            return panel.url?.path
+        }.runOnQueue(.main)
     }
 }
