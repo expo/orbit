abb5ce6c2c5dfb7f4f76a6aa3bc122a456fdefe9
Gabriel Donadel <donadeldev@gmail.com>
2024-02-13 16:47:30 -0300

    Add changelog entry

--- apps/menu-bar/electron/modules/mainRegistry.ts
+++ apps/menu-bar/electron/modules/mainRegistry.ts
@@ -1,12 +1,12 @@
 import { Registry } from 'react-native-electron-modules';
 
+import AlertModule from './Alert/main';
 import AutoResizerRootViewManager from './AutoResizerRootViewManager/main';
 import Linking from './Linking/main';
 import WindowManager from './WindowManager/main';
 import FilePickerModule from '../../modules/file-picker/electron/main';
 import MenuBarModule from '../../modules/menu-bar/electron/main';
 import RudderModule from '../../modules/rudder/electron/main';
-import AlertModule from './Alert/main';
 
 export const MainModules: Registry = [
   MenuBarModule,
