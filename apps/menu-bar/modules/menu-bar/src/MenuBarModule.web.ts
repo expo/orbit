af737af9bd467d71fc94b9c1548b8f55b3119368
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2024-02-18 01:13:58 -0800

    [menu-bar][electron] Fix MenuBar error message parsing (#182)
    * [menu-bar][electron] Fix MenuBar error message parsing
    
    * Add changelog entry

--- apps/menu-bar/modules/menu-bar/src/MenuBarModule.web.ts
+++ apps/menu-bar/modules/menu-bar/src/MenuBarModule.web.ts
@@ -2,4 +2,25 @@ import { requireElectronModule } from 'react-native-electron-modules/build/requi
 
 import { NativeMenuBarModule } from './types';
 
-export default requireElectronModule<NativeMenuBarModule>('MenuBar');
+const MenuBar = requireElectronModule<NativeMenuBarModule>('MenuBar');
+export default {
+  ...MenuBar,
+  async runCli(...args) {
+    try {
+      return await MenuBar.runCli(...args);
+    } catch (error) {
+      if (error instanceof Error) {
+        /**
+         * Electron adds a prefix to the error message, so we need to filter it out in order
+         * to parse the JSON object.
+         *
+         * e.g. "Error occurred in handler for 'MenuBar:runCli': Error: {message:'Error: Command failed: expo start'}"
+         */
+        const filteredErrorMessage = error.message.substring(error.message.indexOf('Error:') + 7);
+
+        throw new Error(filteredErrorMessage);
+      }
+      throw error;
+    }
+  },
+} as typeof MenuBar;
