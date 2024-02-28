878e11224e5b0c4a1530eb5c490a44bb8b0b8347
Gabriel Donadel <donadeldev@gmail.com>
2024-02-28 17:08:58 -0300

    [menu-bar][electron] Fix CLI vite config

--- apps/menu-bar/electron/vite.cli.config.ts
+++ apps/menu-bar/electron/vite.cli.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  resolve: {
+    browserField: false,
+  },
   build: {
     outDir: './.vite/build/cli',
     commonjsOptions: {
       include: [/common-types/, /eas-shared/, /node_modules/],
+    },
+    rollupOptions: {
+      external: ['uuid'],
     },
   },
   optimizeDeps: {
