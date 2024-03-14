b45632155b824958eeb81fae0192d54b156e7516
Gabriel Donadel <donadeldev@gmail.com>
2024-03-14 18:54:30 -0300

    Prevent detectIOSAppType from erroring out

--- packages/eas-shared/src/run/ios/inspectApp.ts
+++ packages/eas-shared/src/run/ios/inspectApp.ts
@@ -1,9 +1,14 @@
 import path from 'path';
+import fs from 'fs-extra';
 
 import { parseBinaryPlistAsync } from '../../utils/parseBinaryPlistAsync';
 
 export async function detectIOSAppType(appPath: string): Promise<'device' | 'simulator'> {
   const builtInfoPlistPath = path.join(appPath, 'Info.plist');
+  if (!fs.existsSync(builtInfoPlistPath)) {
+    return 'device';
+  }
+
   const { DTPlatformName }: { DTPlatformName: string } =
     await parseBinaryPlistAsync(builtInfoPlistPath);
 
