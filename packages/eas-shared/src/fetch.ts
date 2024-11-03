4c638a155a69fb7a44ab1ac4bbdf61ecac545480
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2024-11-03 20:49:24 -0300

    [eas-shared] Add support for self-signed certificates (#208)
    * [eas-shared] Add support for self-signed certificates
    
    * Add changelog entry

--- packages/eas-shared/src/fetch.ts
+++ packages/eas-shared/src/fetch.ts
@@ -1,5 +1,11 @@
 import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
 export * from 'node-fetch';
+import { systemCertsSync } from 'system-ca';
+import https from 'https';
+
+const agent = new https.Agent({
+  ca: systemCertsSync({ includeNodeCertificates: true }),
+});
 
 export class RequestError extends Error {
   constructor(
@@ -12,6 +18,7 @@ export class RequestError extends Error {
 
 export default async function (url: RequestInfo, init?: RequestInit): Promise<Response> {
   const response = await fetch(url, {
+    agent,
     ...init,
   });
   if (response.status >= 400) {
