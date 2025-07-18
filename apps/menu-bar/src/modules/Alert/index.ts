6f806b113438b82e71961d876adca75b6e8740d4
Douglas Lowder <douglowder@mac.com>
2025-07-18 12:45:52 -0700

    [menu-bar] Limit size of messages in error alerts (#288)
    * [menu-bar] Limit size of messages in error alerts
    
    * Fix lint
    
    * CHANGELOG
    
    * Revert "Fix lint"
    
    This reverts commit 83a6b2bec43c809f179f552e5342b6ec31182fa8.
    
    * Revert "[menu-bar] Limit size of messages in error alerts"
    
    This reverts commit a9ead1adcd4bd1c1c682a6c9fda922e0083820b9.
    
    * Truncate all alerts to 1000 characters

--- apps/menu-bar/src/modules/Alert/index.ts
+++ apps/menu-bar/src/modules/Alert/index.ts
@@ -5,6 +5,14 @@ type MacOSAlertOptions = AlertOptions & {
   critical?: boolean;
 };
 
+export function abbreviatedAlertMessage(message: string) {
+  const maxMessageLength = 1000;
+  const longMessageTrailer = `...\n\n + Messages over ${maxMessageLength} characters are abbreviated.`;
+  return message.length < maxMessageLength
+    ? message
+    : message.substring(0, maxMessageLength - longMessageTrailer.length) + longMessageTrailer;
+}
+
 /**
  * A wrapper around RNAlert to override the
  * default alert and set modal true, that way
@@ -18,7 +26,10 @@ const Alert = {
     buttons?: AlertButton[],
     options: MacOSAlertOptions = {}
   ): void {
-    RNAlert.alert(title, message, buttons, { modal: true, ...options });
+    RNAlert.alert(title, message && abbreviatedAlertMessage(message), buttons, {
+      modal: true,
+      ...options,
+    });
   },
 };
 
