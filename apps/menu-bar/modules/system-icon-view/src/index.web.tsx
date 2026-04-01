37851cf1ef61bef48404c12e09f1258084e7729d
Gabriel Donadel <donadeldev@gmail.com>
2026-04-01 02:03:17 -0300

    Use SystemIconView for builds section

--- apps/menu-bar/modules/system-icon-view/src/index.web.tsx
+++ apps/menu-bar/modules/system-icon-view/src/index.web.tsx
@@ -1,6 +1,8 @@
 import { Bug16Filled, Question16Filled } from '@fluentui/react-icons';
 import { ImageProps } from 'react-native';
 
+import Earth02Icon from '../../../src/assets/icons/earth-02.svg';
+import File05Icon from '../../../src/assets/icons/file-05.svg';
 import IphoneIcon from '../../../src/assets/icons/iphone.svg';
 import TvosIcon from '../../../src/assets/icons/tvos.svg';
 import WatchIcon from '../../../src/assets/icons/watch.svg';
@@ -13,8 +15,12 @@ function SystemIconView({ systemIconName, ...props }: Props) {
       return <Bug16Filled />;
     case 'applewatch':
       return <WatchIcon {...props} />;
+    case 'globe':
+      return <Earth02Icon {...props} stroke={props.tintColor} />;
     case 'iphone':
       return <IphoneIcon {...props} />;
+    case 'text.document':
+      return <File05Icon {...props} stroke={props.tintColor} />;
     case 'tv':
       return <TvosIcon {...props} />;
     default:
